de57d3819d50deff0634e31a39af181b91180206
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2022-09-02 01:39:11 +0430

    add style and tailwind and skeletom css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/style/index.css";
+import "./assets/tailwind.css";
+import "skeleton-screen-css";
 
 Vue.config.productionTip = false;
 
