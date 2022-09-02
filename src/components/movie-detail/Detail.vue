0a111abd6467770d0dbc170553cb923e1aea8c50
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2022-09-02 10:33:11 +0430

    handle component show detail movie

---
+++ src/components/movie-detail/Detail.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="detail mt-20">
+    <ImageCover :path="imagePath" />
+    <List />
+    <Description />
+  </div>
+</template>
+<script>
+import ImageCover from "@/components/movie-detail/detail/ImageCover";
+import List from "@/components/movie-detail/detail/List";
+import Description from "@/components/movie-detail/detail/Description";
+import env from "@/config/env";
+export default {
+  name: "Detail",
+  props: {
+    detailMovie: {
+      type: Object,
+      require: true,
+      default: () => {},
+    },
+  },
+  computed: {
+    imagePath() {
+      return `${env.baseUrlImage}${this.detailMovie?.poster_path}`;
+    },
+  },
+  components: {
+    ImageCover,
+    List,
+    Description,
+  },
+};
+</script>
