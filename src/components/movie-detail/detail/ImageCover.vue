ea42861604747449c7b6d643915dfa5b84fc21df
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2022-09-02 09:30:06 +0430

    handle component show image in detail movie page

---
+++ src/components/movie-detail/detail/ImageCover.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img :src="path" class="w-image h-image rounded-lg" alt="image" />
+  </div>
+</template>
+<script>
+export default {
+  name: "ImageCover",
+  props: {
+    path: {
+      type: String,
+      require: true,
+      default: "",
+    },
+  },
+};
+</script>
+<style>
+.h-image {
+  height: 495px !important;
+}
+.w-image {
+  width: 330px !important;
+}
+</style>
