8470c68e7591abf0c062151da6c661b152298874
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2022-09-02 14:36:21 +0430

    handle description

---
+++ src/components/movie-detail/detail/Description.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="description">
+    <p v-if="info" class="text-justify">
+      {{ info.overview }}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Description",
+  props: {
+    info: {
+      type: Object,
+      require: true,
+      default: () => {},
+    },
+  },
+};
+</script>
