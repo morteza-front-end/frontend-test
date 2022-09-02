1b478fca5195755bf433003bc67e3841c134f468
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2022-09-02 09:28:21 +0430

    handle component datepicker

---
+++ src/components/search/date-picker/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <datepicker
+      class="w-56"
+      placeholder=" "
+      v-model="model.date"
+      :minimumView="'day'"
+      :maximumView="'month'"
+      :initialView="'month'"
+      :format="DatePickerFormat"
+    ></datepicker>
+  </div>
+</template>
+
+<script>
+import Datepicker from "vuejs-datepicker";
+
+export default {
+  components: {
+    Datepicker,
+  },
+
+  data() {
+    return {
+      model: {
+        date: "",
+      },
+      DatePickerFormat: "dd/MM/yyyy",
+    };
+  },
+  watch: {
+    "model.date"(Value) {
+      if (Value) {
+        this.$emit("handleFilterByDate", Value);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.w-56 > div > input {
+  padding: 5px 10px;
+  width: 100%;
+}
+</style>
