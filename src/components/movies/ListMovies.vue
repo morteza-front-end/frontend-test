f2fa38063d112e51c905f485011d967e3abd83af
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2022-09-02 09:28:53 +0430

    handle component list movies

---
+++ src/components/movies/ListMovies.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div v-if="!isLoading">
+      <div
+        v-if="listMovies && listMovies.length > 0"
+        class="grid grid-cols-3 items-center justify-between gap-x-16 gap-y-8 mt-36"
+      >
+        <CardMovie
+          v-for="(movie, index) in listMovies"
+          :key="index"
+          :info="movie"
+        />
+      </div>
+      <div
+        v-else-if="error.length > 0"
+        class="flex items-center justify-center h-screen"
+      >
+        <p class="">{{ error }}</p>
+      </div>
+      <div v-else class="h-screen flex justify-center items-center">
+        <p class="font-bold text-lg">Nothing Found..!</p>
+      </div>
+    </div>
+    <div
+      v-else
+      class="grid grid-cols-3 items-center justify-between gap-x-16 gap-y-8 mt-36"
+    >
+      <div
+        class="ssc-square rounded"
+        v-for="(item, index) in 20"
+        :key="index"
+        :class="item"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardMovie from "../movies/movie-card/index.vue";
+
+export default {
+  name: "ListMovies",
+  components: {
+    CardMovie,
+  },
+  props: {
+    listMovies: {
+      type: Array,
+      require: true,
+      default: () => {},
+    },
+    isLoading: {
+      type: Boolean,
+      require: true,
+      default: false,
+    },
+    error: {
+      type: String,
+      require: false,
+      default: "",
+    },
+  },
+  computed: {
+    // isShowMovies() {
+    //   return this.listMovies?.length > 0;
+    // },
+  },
+};
+</script>
+
+<style>
+.ssc-square {
+  height: 192px !important;
+}
+</style>
