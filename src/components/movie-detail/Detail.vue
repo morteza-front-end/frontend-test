30d9d44887ea13d8597bcfcd54b07c30b2005e64
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2022-09-02 14:37:35 +0430

    handle description and credit api

--- src/components/movie-detail/Detail.vue
+++ src/components/movie-detail/Detail.vue
@@ -4,14 +4,17 @@
       <ImageCover :path="imagePath" />
       <List :info="detailMovie" class="flex-grow" />
     </div>
-    <Description />
+    <Description :info="detailMovie" />
+    <Credit :credit-detail="creditsMovie" />
   </div>
 </template>
 <script>
 import ImageCover from "@/components/movie-detail/detail/ImageCover";
 import List from "@/components/movie-detail/detail/List";
 import Description from "@/components/movie-detail/detail/Description";
+import Credit from "@/components/movie-detail/detail/Credit";
 import env from "@/config/env";
+import axios from "axios";
 export default {
   name: "Detail",
   props: {
@@ -20,6 +23,11 @@ export default {
       require: true,
       default: () => {},
     },
+  },
+  data() {
+    return {
+      creditsMovie: null,
+    };
   },
   computed: {
     imagePath() {
@@ -30,6 +38,22 @@ export default {
     ImageCover,
     List,
     Description,
+    Credit,
+  },
+  async created() {
+    await this.getCredits();
+  },
+  methods: {
+    async getCredits() {
+      let options = {
+        api_key: "f62f750b70a8ef11dad44670cfb6aa57",
+      };
+      let id = this.$route.query.q;
+      const data = await axios.get(env.creditsMovie(id), {
+        params: options,
+      });
+      this.creditsMovie = data?.data;
+    },
   },
 };
 </script>
