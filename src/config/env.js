843418481f0bbc1ed89b29d2d8b1add0fde5affa
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2022-09-02 14:38:01 +0430

    add credit api

--- src/config/env.js
+++ src/config/env.js
@@ -2,4 +2,6 @@ export default {
   baseUrl: "https://api.themoviedb.org/3/discover",
   detailMovie: "https://api.themoviedb.org/3/movie/",
   baseUrlImage: "https://image.tmdb.org/t/p/w500",
+  creditsMovie: (movieId) =>
+    `https://api.themoviedb.org/3/movie/${movieId}/credits`,
 };
